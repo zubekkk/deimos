d05d7a1bfe7f43b266526ecc3de11a036a7f84f7
Mango19 <143813812+zubekkk@users.noreply.github.com>
2026-07-17 01:54:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const serverIp = document.getElementById('server-ip');
 const serverPreset = document.getElementById('server-preset');
 
 const PROXIES = [
-    url => `https://corsproxy.io/?${encodeURIComponent(url)}`,
+    url => `https://corsproxy.io/?url=${encodeURIComponent(url)}`,
     url => `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`,
     url => `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`,
 ];
